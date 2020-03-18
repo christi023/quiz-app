29abbf3b1e2415fca5296fa5247880b212a7e9c3
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-03-18 11:20:48 +0100

    Added Questions

--- src/Components/Quiz/Play.jsx
+++ src/Components/Quiz/Play.jsx
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 
+import questions from '../../questions.json';
+import isEmpty from '../../utils/is-empty';
+
 export default class Play extends Component {
-  /*constructor(props) {
+  constructor(props) {
     super(props);
-    this.state = {}
-  }*/
+    this.state = {
+      questions,
+      currentQuestion: {},
+      nextQuestion: {},
+      previousQuestion: {},
+      answer: '',
+      numberOfQuestions: 0,
+      numberOfAnsweredQuestion: 0,
+      numberOfUnansweredQuestion: 0,
+      currentQuestionIndex: 0,
+      score: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      hints: 5,
+      fiftyFifty: 2,
+      usedFiftyFifty: false,
+      time: {},
+    };
+  }
+
+  //
+  componentDidMount() {
+    const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state;
+    this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
+  }
+
+  // display questions method
+  displayQuestions = (
+    questions = this.state.questions,
+    currentQuestion,
+    nextQuestion,
+    previousQuestion,
+  ) => {
+    let { currentQuestionIndex } = this.state;
+    if (!isEmpty(this.state.questions)) {
+      questions = this.state.questions;
+      currentQuestion = questions[currentQuestionIndex];
+      nextQuestion = questions[currentQuestionIndex + 1];
+      previousQuestion = questions[currentQuestion - 1];
+      const answer = currentQuestion.answer;
+      this.setState({
+        currentQuestion,
+        nextQuestion,
+        previousQuestion,
+        answer,
+      });
+    }
+  };
 
   render() {
+    const { currentQuestion } = this.state;
     return (
       <>
         <Helmet>Quiz Page</Helmet>
@@ -34,14 +84,14 @@ export default class Play extends Component {
               </span>
             </p>
           </div>
-          <h5>Google was founded in what year?</h5>
+          <h5>{currentQuestion.question}</h5>
           <div className="options-container">
-            <p className="option"> 1997</p>
-            <p className="option"> 1998</p>
+            <p className="option"> {currentQuestion.optionA}</p>
+            <p className="option">{currentQuestion.optionB} </p>
           </div>
           <div className="options-container">
-            <p className="option"> 1999</p>
-            <p className="option"> 2000</p>
+            <p className="option">{currentQuestion.optionC} </p>
+            <p className="option">{currentQuestion.optionD}</p>
           </div>
           <div className="button-container">
             <button>Previous</button>
